7c34069c80be59c4fd9094da976774a38e2df097
theburningmonk@gmail.com <theburningmonk@gmail.com>
2014-06-26 20:48:50 +0100

    - added LogTime workflow - added example for LogTime workflow

---
+++ src/SharpRavenContribFs/LogTimeWorkflow.fsi
@@ -0,0 +1,12 @@
+﻿namespace SharpRaven
+
+open System
+
+[<AutoOpen>]
+module LogTimeWorkflow =
+    [<Sealed>]
+    type LogTimeBuilder = 
+        member Delay : (unit -> 'a) -> 'a
+        member Zero  : unit -> unit
+
+    val logTime : RavenClient * TimeSpan * string -> LogTimeBuilder
--- src/SharpRavenContribFs/RavenWorkflow.fs
+++ src/SharpRavenContribFs/RavenWorkflow.fs
@@ -2,16 +2,15 @@
 
 [<AutoOpen>]
 module RavenWorkflow =
-    let delay f (client : RavenClient) =
-        try
-            f()
-        with
-        | ex -> client.CaptureException(ex) |> ignore
-                reraise()
-
     [<Sealed>]
     type RavenBuilder internal (client : RavenClient) =
-        member b.Delay f        = delay f client
-        member b.Zero ()        = ()
+        member b.Delay f = 
+            try
+                f()
+            with
+            | ex -> client.CaptureException(ex) |> ignore
+                    reraise()
+
+        member b.Zero () = ()
 
     let raven client = new RavenBuilder(client)
